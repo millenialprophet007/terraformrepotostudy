443b28bda6708fdb8f19b254f9e3fcfe5c309be2
Shihab Saheer <shihabshaheer8@gmail.com>
2025-02-12 23:06:10 +0530

    New resource created and code updated

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "local_file" "testfile1" {
   filename = var.filename
   content = var.filename
 }
+
+resource "local_file" "testfile2" {
+  filename = var.filename2
+  content = var.content2
+}
